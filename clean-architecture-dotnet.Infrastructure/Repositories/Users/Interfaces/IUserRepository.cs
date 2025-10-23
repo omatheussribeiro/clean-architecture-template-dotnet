5ff85d312352f7d19606695e34bf9c28b0eb05a6
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-10-23 11:36:59 -0300

    created the first repositories

---
+++ clean-architecture-dotnet.Infrastructure/Repositories/Users/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using clean_architecture_dotnet.Domain.Entities.Users;
+
+namespace clean_architecture_dotnet.Infrastructure.Repositories.Users.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(int id);
+        Task<User> Put(User user);
+        Task<User> Post(User user);
+        Task<User> Delete(User user);
+    }
+}
