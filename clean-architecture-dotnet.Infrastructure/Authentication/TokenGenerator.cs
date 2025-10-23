f2283bd966041c3832fbc358107b8dbd332d7866
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-10-23 10:48:30 -0300

    Created jwt token configuration

---
+++ clean-architecture-dotnet.Infrastructure/Authentication/TokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace clean_architecture_dotnet.Infrastructure.Authentication
+{
+    public class TokenGenerator
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public TokenGenerator(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(string email)
+        {
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.SecretKey);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, email)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(_jwtSettings.ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
